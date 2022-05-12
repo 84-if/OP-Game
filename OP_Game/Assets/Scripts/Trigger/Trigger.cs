64ca4078646ef02f45c9d86bf95007c2441a5fd5
nnvslv <annavromanovna@gmail.com>
2022-05-12 23:43:10 +0500

    added dialogue system

--- OP_Game/Assets/Scripts/Trigger/Trigger.cs
+++ OP_Game/Assets/Scripts/Trigger/Trigger.cs
@@ -1,5 +1,8 @@
+using Dialogues;
 using MainCamera;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 namespace Trigger
 {
@@ -18,11 +21,25 @@ namespace Trigger
         {
             if(other.CompareTag("Player") && Input.GetKeyDown(KeyCode.E))
             {
-                other.gameObject.transform.position = new Vector3(1.194f, -0.063f, 0f);
-                chase.leftLimit = 1.009f + 0.9575f;
-                chase.rightLimit = 5.17f - 0.958f;
-                chase.upperLimit = 0.8f - 0.47f;
-                chase.bottomLimit = -0.48f + 0.47f;
+                var dialogueWindow = GameObject.FindGameObjectWithTag("DialogueWindow").GetComponent<Image>();
+                var dialogueText = GameObject.FindGameObjectWithTag("DialogueText").GetComponent<TextMeshProUGUI>();
+                var textScript = GameObject.FindGameObjectWithTag("DialogueText").GetComponent<DialogueSystem>();
+                dialogueWindow.enabled = true;
+                dialogueText.enabled = true;
+                textScript.enabled = true;
+                textScript.lines = new [] {"Мам, я пошел", "Давай, сынок", "Пап.. Можно машину взять? Я тихонько.",
+                    "Жирно будет", "Ну ладно", "Будь аккуратнее, не задерживайся.", "Да да да…"};
+                if (!textScript.isTalking)
+                {
+                    dialogueWindow.enabled = false;
+                    dialogueText.enabled = false;
+                    textScript.enabled = false;
+                    other.gameObject.transform.position = new Vector3(1.194f, -0.063f, 0f);
+                    chase.leftLimit = 1.009f + 0.9575f;
+                    chase.rightLimit = 5.17f - 0.958f;
+                    chase.upperLimit = 0.8f - 0.47f;
+                    chase.bottomLimit = -0.48f + 0.47f;
+                }
             }
         }  
     }
