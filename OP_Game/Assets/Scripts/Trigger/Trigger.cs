1626dc09cfc20096ec73e40d075da6d8cc5e1e75
3a4eM-MHE-HuK <gavrilov.nikita@urfu.me>
2022-05-22 21:49:04 +0500

    Fixed movement anim. Added all levels Fixed camera

--- OP_Game/Assets/Scripts/Trigger/Trigger.cs
+++ OP_Game/Assets/Scripts/Trigger/Trigger.cs
@@ -37,8 +37,8 @@ namespace Trigger
                     other.gameObject.transform.position = new Vector3(1.194f, -0.063f, 0f);
                     chase.leftLimit = 1.009f + 0.9575f;
                     chase.rightLimit = 5.17f - 0.958f;
-                    chase.upperLimit = 0.8f - 0.47f;
-                    chase.bottomLimit = -0.48f + 0.47f;
+                    chase.upperLimit = 0.5f;
+                    chase.bottomLimit = -0.038f;
                 }
             }
         }  
