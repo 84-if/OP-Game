39df8391a0507559a04a1eb4de70a5bcb33569b4
3a4eM-MHE-HuK <gavrilov.nikita@urfu.me>
2022-05-30 20:10:06 +0500

    Added infinity scrolling of road

---
+++ OP_Game/Assets/Scroll.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour
+{
+    public float speed = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var offset = new Vector2(Time.time * speed, 0);
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+    }
+}
