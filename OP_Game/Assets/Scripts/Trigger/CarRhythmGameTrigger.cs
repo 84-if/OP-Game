b976b62f901dd6a9eb8dfae5ba6f5aa20cd83dc6
3a4eM-MHE-HuK <gavrilov.nikita@urfu.me>
2022-06-01 00:47:03 +0500

    Do not know why car rhythm game is not working

--- OP_Game/Assets/Scripts/Trigger/CarRhythmGameTrigger.cs
+++ OP_Game/Assets/Scripts/Trigger/CarRhythmGameTrigger.cs
@@ -10,14 +10,15 @@ namespace Trigger
     public class CarRhythmGameTrigger : MonoBehaviour
     {
         private readonly List<string> _objectsToEnable = new() {"Horizontal Audio", "Horizontal Buttons", 
-            "Horizontal ScoreText", "Horizontal NoteHolder", "Horizontal GameManager", "Horizontal GamesPlus"};
+            "Horizontal ScoreText", "Horizontal NoteHolder", "Horizontal GameManager", "Horizontal JamesPlus"};
         private void OnTriggerStay2D (Collider2D other)
         {
             var dialogueWindow = GameObject.FindGameObjectWithTag("DialogueWindow").GetComponent<Image>();
             var dialogueText = GameObject.FindGameObjectWithTag("DialogueText").GetComponent<TextMeshProUGUI>();
             var dialogueNames = GameObject.FindGameObjectWithTag("DialogueNames").GetComponent<TextMeshProUGUI>();
             var textScript = GameObject.FindGameObjectWithTag("DialogueText").GetComponent<DialogueSystem>();
-            if(other.CompareTag("Car"))
+            var flag = GameObject.FindGameObjectWithTag("jopa").GetComponent<SpriteRenderer>();
+            if(other.CompareTag("Car") && flag.enabled)
             {
                 dialogueWindow.enabled = true;
                 dialogueText.enabled = true;
@@ -29,6 +30,7 @@ namespace Trigger
                     "Кирюха", "Тачка зачёт!",
                     "Серёга", "Поддай газку, дружище! Чо еле плетёмся?!"
                 };
+                flag.enabled = false;
             }
             if (other.CompareTag("Car") && !textScript.isTalking)
             {
@@ -55,28 +57,36 @@ namespace Trigger
                     case "Horizontal Audio":
                         var track = GameObject.FindGameObjectWithTag(objectTag).GetComponent<AudioSource>();
                         track.enabled = true;
+                        Debug.Log("Enable audio");
                         break;
                     case "Horizontal Buttons":
                         var buttons = GameObject.FindGameObjectWithTag(objectTag).GetComponent<Transform>();
                         buttons.transform.position = new Vector3(buttons.transform.position.x, 
-                            buttons.transform.position.y, 50f);
+                            buttons.transform.position.y, 0f);
+                        Debug.Log("Enable buttons");
                         break;
                     case "Horizontal ScoreText":
                         var scoreText = GameObject.FindGameObjectWithTag(objectTag).GetComponent<Text>();
                         scoreText.enabled = true;
+                        Debug.Log("Enable scoretext");
                         break;
                     case "Horizontal NoteHolder":
                         var noteHolder = GameObject.FindGameObjectWithTag(objectTag)
                             .GetComponent<BeatScroller>();
                         noteHolder.enabled = true;
+                        Debug.Log("Enable noteholder");
                         break;
                     case "Horizontal GameManager":
                         var gameManager = GameObject.FindGameObjectWithTag(objectTag).GetComponent<GameManager>();
                         gameManager.enabled = true;
+                        Debug.Log("Enable gamemanager");
+
                         break;
-                    case "Horizontal GamesPlus":
+                    case "Horizontal JamesPlus":
                         var gamesPlus = GameObject.FindGameObjectWithTag(objectTag).GetComponent<AudioSource>();
                         gamesPlus.enabled = true;
+                        Debug.Log("Enable jamesplus");
+
                         break;
                 }
             }
