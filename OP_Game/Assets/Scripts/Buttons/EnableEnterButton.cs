59be0a331ae68764a49c9fe0c18c53fd51355518
3a4eM-MHE-HuK <gavrilov.nikita@urfu.me>
2022-06-01 17:43:21 +0500

    Fix hints omt

--- OP_Game/Assets/Scripts/Buttons/EnableEnterButton.cs
+++ OP_Game/Assets/Scripts/Buttons/EnableEnterButton.cs
@@ -8,19 +8,29 @@ namespace Buttons
     {
         public Image dialogueWindow;
         public SpriteRenderer enterButton;
+        public SpriteRenderer carEnterButton;
+        
         void Start()
         {
             dialogueWindow = GameObject.FindGameObjectWithTag("DialogueWindow").GetComponent<Image>();
             enterButton = GameObject.FindGameObjectWithTag("Enter Button").GetComponent<SpriteRenderer>();
+            carEnterButton = GameObject.FindGameObjectWithTag("Car Enter Button").GetComponent<SpriteRenderer>();
         }
 
         // Update is called once per frame
         void Update()
         {
             if (dialogueWindow.enabled)
+            {
                 enterButton.enabled = true;
+                carEnterButton.enabled = true;
+            }
             else
+            {
                 enterButton.enabled = false;
+                carEnterButton.enabled = false;
+            }
+                
 
         }
     }
